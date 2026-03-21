c20f4cac70e249361b7292ac83e142e1f148d9ac
frostkom <martin.fors@frostkom.se>
2026-03-21 08:29:19 +0100

    Fix bug after testing code removal

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Webshop
 Plugin URI: https://github.com/frostkom/mf_webshop
 Description: Adds functionality for a webshop
-Version: 2.2.8.12
+Version: 2.2.8.13
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
